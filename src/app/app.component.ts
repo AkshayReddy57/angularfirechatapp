39dc3ea9374c0164447ca32e1a220a53284bd16f
unknown <akshay.patlolla@caerusskillmatic.com>
2018-08-19 10:17:10 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,17 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { map } from 'rxjs/operators';
+import { Observable } from '../../node_modules/rxjs';
+
+interface Post {
+  title: string;
+  content: string;
+}
+
+
+interface PostId extends Post { 
+  id: string; 
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +19,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'chat';
+
+
+
+  postsCol: AngularFirestoreCollection<Post>;
+  posts: any;
+
+
+  title:string;
+  content:string;
+
+
+  postDoc: AngularFirestoreDocument<Post>;
+  post: Observable<Post>;
+
+  constructor(private afs: AngularFirestore) {
+
+  }
+
+  ngOnInit() {
+    this.postsCol = this.afs.collection('posts');
+    // this.posts = this.postsCol.valueChanges();
+    this.posts = this.postsCol.valueChanges()
+
+  }
+
+  addPost() {
+    // this.afs.collection('posts').add({'title': this.title, 'content': this.content});
+    this.afs.collection('posts').add({'title': this.title, 'content': this.content});
+  }
+
+  getPost(postId) {
+    this.postDoc = this.afs.doc('posts/'+postId);
+    this.post = this.postDoc.valueChanges();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+import { FormsModule } from '@angular/forms';
+
+var firebaseConfig = {
+
+  apiKey: "AIzaSyC47Dl5ZLFcm4YQD0FXvhhXoKLP5A4xh-M",
+  authDomain: "firestore-c7392.firebaseapp.com",
+  databaseURL: "https://firestore-c7392.firebaseio.com",
+  projectId: "firestore-c7392",
+  storageBucket: "firestore-c7392.appspot.com",
+  messagingSenderId: "612582241978"
+
+}
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
